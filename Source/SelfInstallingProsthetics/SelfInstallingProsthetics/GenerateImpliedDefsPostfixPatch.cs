4bfbe34a8438ad9761b2dd00500cfde92addbb55
KonradHeinser <robhos2000@gmail.com>
2025-05-29 13:52:33 -0700

    Another fix attempt

---
+++ Source/SelfInstallingProsthetics/SelfInstallingProsthetics/GenerateImpliedDefsPostfixPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace SelfInstallingProsthetics
+{
+    [HarmonyPatch(typeof(DefGenerator))]
+    [HarmonyPatch("GenerateImpliedDefs_PreResolve")]
+    public static class GenerateImpliedDefsPostfixPatch
+    {
+        [HarmonyPrefix]
+        public static void GenerateImpliedDefsPostfix(bool hotReload = false)
+        {
+            foreach (ThingDef sip in ThingDefGenerator_SelfInstallingProsthetic.ImpliedThingDefs(hotReload))
+                DefGenerator.AddImpliedDef(sip);
+        }
+    }
+
+}
---
+++ Source/SelfInstallingProsthetics/SelfInstallingProsthetics/SelfInstallingProstheticsMod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using HarmonyLib;
+
+namespace SelfInstallingProsthetics
+{
+    public class SelfInstallingProstheticsMod : Mod
+    {
+        public SelfInstallingProstheticsMod(ModContentPack content) : base(content)
+        {
+            harmony = new Harmony("Rimworld.Alite.SelfInstallingProsthetics");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public static Harmony harmony;
+    }
+}
